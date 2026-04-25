cd36f194416b645eda8e781d29bdae12cbec8d1f
s3vro-h1 <s3vro.howler1@gmail.com>
2026-04-25 12:24:02 -0600

    Reset: Solo README, License, config.json, index.html y resources

--- config.py
+++
@@ -1,3 +0,0 @@
-#Check the readme to see how the path should look like on each OS
-SONGS_PATH ="/home/yourname/Clone Hero/Songs" 
-OS = "LINUX"
--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 1. Generar el nuevo JSON
-python3 generate_list.py
-
-# 2. Subir a GitHub
-git add data.json genres.json config.json
-git commit -m "Update song list: $(date)"
-git push origin main
